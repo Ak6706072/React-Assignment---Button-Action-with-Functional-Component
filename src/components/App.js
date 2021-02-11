465ef07d37d256b77a280e25bef1c545725133da
Ashish kumar <ak6706072@gmail.com>
2021-02-12 04:02:38 +0530

    making functionality with conditionsal rendering in functional component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,22 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [flag,setFlag] = useState(false);
+  const handleClick = ()=>{
+    setFlag(true);
+  }
+  const render = ()=>{
+    if(flag){
+      return <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+    }else{
+      return <button id="click" onClick={handleClick}>Click</button>
+    }
+  }
   return (
     <div id="main">
       // Do not alter the main div
+      {render()}
+
     </div>
   );
 }
